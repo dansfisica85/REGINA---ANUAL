732e9a732c83a725e71fafbad2f2a125d6213004
DAVI ANTONINO NUNES DA SILVA <118570287+dansfisica85@users.noreply.github.com>
2025-12-18 20:07:44 +0000

    Atualiza dados de uso das plataformas Matific e Speak para 2025

--- regina-site/src/data/matific.js
+++ regina-site/src/data/matific.js
@@ -7,182 +7,182 @@ export const matificData = [
     tipo: "Regular",
     turno: "1 T",
     alunos: 50,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    bimestres: { b1: 4.99, b2: 6.29, b3: 3.71, b4: 3.07 }
   },
   {
     name: "PEI EE Maria Falconi de Felício",
     tipo: "PEI",
     turno: "1 T",
     alunos: 396,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "PEI EE Profª Dolores B Novaes",
-    tipo: "PEI",
-    turno: "1 T",
-    alunos: 503,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "PEI EE DR Antonio Furlan Júnior",
-    tipo: "PEI",
-    turno: "2 T",
-    alunos: 521,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "PEI EE Domingos Paro",
-    tipo: "PEI",
-    turno: "2 T",
-    alunos: 254,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Winston Churchill",
-    tipo: "Regular",
-    turno: "1 T",
-    alunos: 386,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Orminda Guimarães Cotrim",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 779,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    bimestres: { b1: 3.03, b2: 8.33, b3: 8.65, b4: 5.25 }
   },
   {
     name: "EE Mauricio Montecchi",
     tipo: "Regular",
     turno: "3 T",
     alunos: 815,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    bimestres: { b1: 2.64, b2: 6.18, b3: 5.14, b4: 3.17 }
   },
   {
-    name: "EE Profº Nestor G de Araújo",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 306,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profª Maria Elyde M dos Santos",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 229,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profº Plínio Berardo",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 1038,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Luiz Marcari",
-    tipo: "Regular",
-    turno: "1 T",
-    alunos: 249,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profª Nícia Fabiola Z. Giraldi",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 769,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Anna Passamonti Balardin",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 968,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "PEI EE José Luiz de Siqueira",
+    name: "PEI EE DR Antonio Furlan Júnior",
     tipo: "PEI",
     turno: "2 T",
-    alunos: 742,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    alunos: 521,
+    bimestres: { b1: 2.62, b2: 4.79, b3: 4.16, b4: 1.61 }
   },
   {
-    name: "EE Profº Bruno Pieroni",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 645,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE DR Mário Lins",
-    tipo: "Regular",
+    name: "PEI EE Domingos Paro",
+    tipo: "PEI",
     turno: "2 T",
-    alunos: 105,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    alunos: 254,
+    bimestres: { b1: 2.38, b2: 4.89, b3: 4.95, b4: 2.25 }
   },
   {
-    name: "EE DR Isaías José Ferreira",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 631,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profª Edith Silveira Dalmaso",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 1058,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profª Dolores M de Castro",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 148,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profª Yolanda Luiz Sicchieri",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 642,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profº Basílio R da Silva",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 571,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Ferrucio Chiaratti",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 902,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Odulfo de Oliveira Guimarães",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 897,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profª Josepha Castro",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 527,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    name: "PEI EE Profª Dolores B Novaes",
+    tipo: "PEI",
+    turno: "1 T",
+    alunos: 503,
+    bimestres: { b1: 2.25, b2: 5.37, b3: 5.09, b4: 5.42 }
   },
   {
     name: "EE Profª Maria C R S Magon",
     tipo: "PEI",
     turno: "2 T",
     alunos: 236,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    bimestres: { b1: 2.15, b2: 9.09, b3: 6.19, b4: 3.83 }
+  },
+  {
+    name: "EE Profº Basílio R da Silva",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 571,
+    bimestres: { b1: 2.06, b2: 2.94, b3: 3.2, b4: 1.4 }
+  },
+  {
+    name: "EE Orminda Guimarães Cotrim",
+    tipo: "Regular",
+    turno: "2 T",
+    alunos: 779,
+    bimestres: { b1: 1.92, b2: 3.25, b3: 3.47, b4: 1.98 }
+  },
+  {
+    name: "EE Profª Edith Silveira Dalmaso",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 1058,
+    bimestres: { b1: 1.79, b2: 3.58, b3: 2.3, b4: 0.38 }
+  },
+  {
+    name: "EE Profª Dolores M de Castro",
+    tipo: "Regular",
+    turno: "2 T",
+    alunos: 148,
+    bimestres: { b1: 1.54, b2: 3.0, b3: 4.37, b4: 4.91 }
+  },
+  {
+    name: "EE Profª Josepha Castro",
+    tipo: "Regular",
+    turno: "2 T",
+    alunos: 527,
+    bimestres: { b1: 1.53, b2: 2.67, b3: 3.13, b4: 0.98 }
+  },
+  {
+    name: "EE Odulfo de Oliveira Guimarães",
+    tipo: "Regular",
+    turno: "2 T",
+    alunos: 897,
+    bimestres: { b1: 1.43, b2: 2.78, b3: 3.82, b4: 2.11 }
+  },
+  {
+    name: "EE Anna Passamonti Balardin",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 968,
+    bimestres: { b1: 1.36, b2: 2.72, b3: 2.87, b4: 1.49 }
+  },
+  {
+    name: "EE Profª Nícia Fabiola Z. Giraldi",
+    tipo: "Regular",
+    turno: "2 T",
+    alunos: 769,
+    bimestres: { b1: 1.18, b2: 2.08, b3: 1.74, b4: 1.3 }
+  },
+  {
+    name: "EE DR Isaías José Ferreira",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 631,
+    bimestres: { b1: 1.12, b2: 1.86, b3: 1.71, b4: 1.2 }
+  },
+  {
+    name: "EE Ferrucio Chiaratti",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 902,
+    bimestres: { b1: 0.89, b2: 2.07, b3: 2.55, b4: 1.85 }
+  },
+  {
+    name: "EE Winston Churchill",
+    tipo: "Regular",
+    turno: "1 T",
+    alunos: 386,
+    bimestres: { b1: 0.0, b2: 0.0, b3: 0.0, b4: 0.0 }
+  },
+  {
+    name: "PEI EE José Luiz de Siqueira",
+    tipo: "PEI",
+    turno: "2 T",
+    alunos: 742,
+    bimestres: { b1: 0.0, b2: 0.0, b3: 0.0, b4: 0.0 }
+  },
+  {
+    name: "EE Profª Maria Elyde M dos Santos",
+    tipo: "PEI",
+    turno: "2 T",
+    alunos: 229,
+    bimestres: { b1: 0.0, b2: 0.0, b3: 0.0, b4: 0.0 }
+  },
+  {
+    name: "EE Profº Bruno Pieroni",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 645,
+    bimestres: { b1: 0.0, b2: 0.0, b3: 0.0, b4: 0.0 }
+  },
+  {
+    name: "EE Profº Nestor G de Araújo",
+    tipo: "PEI",
+    turno: "2 T",
+    alunos: 306,
+    bimestres: { b1: 0.0, b2: 0.0, b3: 0.0, b4: 0.0 }
+  },
+  {
+    name: "EE Profº Plínio Berardo",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 1038,
+    bimestres: { b1: 0.0, b2: 0.0, b3: 0.0, b4: 0.0 }
+  },
+  {
+    name: "EE Luiz Marcari",
+    tipo: "Regular",
+    turno: "1 T",
+    alunos: 249,
+    bimestres: { b1: 0.0, b2: 0.0, b3: 0.0, b4: 0.0 }
+  },
+  {
+    name: "EE Profª Yolanda Luiz Sicchieri",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 642,
+    bimestres: { b1: 0.0, b2: 0.0, b3: 0.0, b4: 0.0 }
+  },
+  {
+    name: "EE DR Mário Lins",
+    tipo: "Regular",
+    turno: "2 T",
+    alunos: 105,
+    bimestres: { b1: 0.0, b2: 0.0, b3: 0.0, b4: 0.0 }
   },
 ];
 
--- regina-site/src/data/speak.js
+++ regina-site/src/data/speak.js
@@ -3,186 +3,186 @@
 
 export const speakData = [
   {
-    name: "EE Dona Adélia Frascino",
-    tipo: "Regular",
-    turno: "1 T",
-    alunos: 50,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "PEI EE Maria Falconi de Felício",
-    tipo: "PEI",
-    turno: "1 T",
-    alunos: 396,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "PEI EE Profª Dolores B Novaes",
-    tipo: "PEI",
-    turno: "1 T",
-    alunos: 503,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "PEI EE DR Antonio Furlan Júnior",
-    tipo: "PEI",
-    turno: "2 T",
-    alunos: 521,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "PEI EE Domingos Paro",
-    tipo: "PEI",
-    turno: "2 T",
-    alunos: 254,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Winston Churchill",
-    tipo: "Regular",
-    turno: "1 T",
-    alunos: 386,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Orminda Guimarães Cotrim",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 779,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Mauricio Montecchi",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 815,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profº Nestor G de Araújo",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 306,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profª Maria Elyde M dos Santos",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 229,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profº Plínio Berardo",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 1038,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
     name: "EE Luiz Marcari",
     tipo: "Regular",
     turno: "1 T",
     alunos: 249,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profª Nícia Fabiola Z. Giraldi",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 769,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Anna Passamonti Balardin",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 968,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    bimestres: { b1: 2.32, b2: 2.95, b3: 2.39, b4: 0.45 }
   },
   {
     name: "PEI EE José Luiz de Siqueira",
     tipo: "PEI",
     turno: "2 T",
     alunos: 742,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    bimestres: { b1: 1.63, b2: 1.52, b3: 1.59, b4: 0.4 }
   },
   {
-    name: "EE Profº Bruno Pieroni",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 645,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE DR Mário Lins",
-    tipo: "Regular",
+    name: "EE Profª Maria Elyde M dos Santos",
+    tipo: "PEI",
     turno: "2 T",
-    alunos: 105,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    alunos: 229,
+    bimestres: { b1: 1.55, b2: 1.44, b3: 1.34, b4: 0.5 }
   },
   {
-    name: "EE DR Isaías José Ferreira",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 631,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profª Edith Silveira Dalmaso",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 1058,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profª Dolores M de Castro",
-    tipo: "Regular",
+    name: "PEI EE DR Antonio Furlan Júnior",
+    tipo: "PEI",
     turno: "2 T",
-    alunos: 148,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    alunos: 521,
+    bimestres: { b1: 1.47, b2: 1.15, b3: 1.17, b4: 0.24 }
   },
   {
-    name: "EE Profª Yolanda Luiz Sicchieri",
+    name: "PEI EE Profª Dolores B Novaes",
+    tipo: "PEI",
+    turno: "1 T",
+    alunos: 503,
+    bimestres: { b1: 1.31, b2: 0.85, b3: 0.99, b4: 0.83 }
+  },
+  {
+    name: "EE Mauricio Montecchi",
     tipo: "Regular",
     turno: "3 T",
-    alunos: 642,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    alunos: 815,
+    bimestres: { b1: 1.15, b2: 0.99, b3: 0.85, b4: 0.49 }
   },
   {
-    name: "EE Profº Basílio R da Silva",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 571,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Ferrucio Chiaratti",
-    tipo: "Regular",
-    turno: "3 T",
-    alunos: 902,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Odulfo de Oliveira Guimarães",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 897,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
-  },
-  {
-    name: "EE Profª Josepha Castro",
-    tipo: "Regular",
-    turno: "2 T",
-    alunos: 527,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    name: "PEI EE Maria Falconi de Felício",
+    tipo: "PEI",
+    turno: "1 T",
+    alunos: 396,
+    bimestres: { b1: 1.12, b2: 0.78, b3: 0.66, b4: 0.15 }
   },
   {
     name: "EE Profª Maria C R S Magon",
     tipo: "PEI",
     turno: "2 T",
     alunos: 236,
-    bimestres: { b1: 0, b2: 0, b3: 0, b4: 0 }
+    bimestres: { b1: 1.06, b2: 1.01, b3: 1.19, b4: 0.26 }
+  },
+  {
+    name: "EE Profº Bruno Pieroni",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 645,
+    bimestres: { b1: 0.95, b2: 1.02, b3: 0.81, b4: 0.35 }
+  },
+  {
+    name: "EE Orminda Guimarães Cotrim",
+    tipo: "Regular",
+    turno: "2 T",
+    alunos: 779,
+    bimestres: { b1: 0.88, b2: 0.96, b3: 0.9, b4: 0.31 }
+  },
+  {
+    name: "EE Profº Plínio Berardo",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 1038,
+    bimestres: { b1: 0.88, b2: 0.7, b3: 0.61, b4: 0.16 }
+  },
+  {
+    name: "EE DR Mário Lins",
+    tipo: "Regular",
+    turno: "2 T",
+    alunos: 105,
+    bimestres: { b1: 0.86, b2: 1.17, b3: 1.1, b4: 0.2 }
+  },
+  {
+    name: "EE Anna Passamonti Balardin",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 968,
+    bimestres: { b1: 0.84, b2: 0.64, b3: 0.61, b4: 0.27 }
+  },
+  {
+    name: "PEI EE Domingos Paro",
+    tipo: "PEI",
+    turno: "2 T",
+    alunos: 254,
+    bimestres: { b1: 0.8, b2: 0.73, b3: 0.91, b4: 0.24 }
+  },
+  {
+    name: "EE Profª Yolanda Luiz Sicchieri",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 642,
+    bimestres: { b1: 0.8, b2: 0.72, b3: 0.68, b4: 0.26 }
+  },
+  {
+    name: "EE Profª Edith Silveira Dalmaso",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 1058,
+    bimestres: { b1: 0.79, b2: 0.65, b3: 0.62, b4: 0.08 }
+  },
+  {
+    name: "EE Profº Nestor G de Araújo",
+    tipo: "PEI",
+    turno: "2 T",
+    alunos: 306,
+    bimestres: { b1: 0.79, b2: 0.92, b3: 0.89, b4: 0.35 }
+  },
+  {
+    name: "EE Winston Churchill",
+    tipo: "Regular",
+    turno: "1 T",
+    alunos: 386,
+    bimestres: { b1: 0.78, b2: 0.85, b3: 0.67, b4: 0.15 }
+  },
+  {
+    name: "EE Profª Nícia Fabiola Z. Giraldi",
+    tipo: "Regular",
+    turno: "2 T",
+    alunos: 769,
+    bimestres: { b1: 0.71, b2: 0.68, b3: 0.62, b4: 0.18 }
+  },
+  {
+    name: "EE Dona Adélia Frascino",
+    tipo: "Regular",
+    turno: "1 T",
+    alunos: 50,
+    bimestres: { b1: 0.68, b2: 0.42, b3: 0.42, b4: 0.2 }
+  },
+  {
+    name: "EE DR Isaías José Ferreira",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 631,
+    bimestres: { b1: 0.67, b2: 0.71, b3: 0.62, b4: 0.32 }
+  },
+  {
+    name: "EE Profª Josepha Castro",
+    tipo: "Regular",
+    turno: "2 T",
+    alunos: 527,
+    bimestres: { b1: 0.59, b2: 0.71, b3: 0.64, b4: 0.15 }
+  },
+  {
+    name: "EE Profº Basílio R da Silva",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 571,
+    bimestres: { b1: 0.59, b2: 0.57, b3: 0.56, b4: 0.21 }
+  },
+  {
+    name: "EE Ferrucio Chiaratti",
+    tipo: "Regular",
+    turno: "3 T",
+    alunos: 902,
+    bimestres: { b1: 0.53, b2: 0.55, b3: 0.55, b4: 0.21 }
+  },
+  {
+    name: "EE Odulfo de Oliveira Guimarães",
+    tipo: "Regular",
+    turno: "2 T",
+    alunos: 897,
+    bimestres: { b1: 0.49, b2: 0.54, b3: 0.5, b4: 0.26 }
+  },
+  {
+    name: "EE Profª Dolores M de Castro",
+    tipo: "Regular",
+    turno: "2 T",
+    alunos: 148,
+    bimestres: { b1: 0.45, b2: 0.52, b3: 0.74, b4: 0.4 }
   },
 ];
 
